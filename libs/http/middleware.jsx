beb09318c90118ade34b0a73e658aa05737640b5
Christian de Botton <cdebotton@gmail.com>
2017-05-20 17:25:55 -0400

    Create koa middleware to render app

---
+++ libs/http/middleware.jsx
@@ -0,0 +1,36 @@
+/* @flow */
+
+import React from 'react';
+import { renderToStaticMarkup, renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import Router from 'react-router/StaticRouter';
+import Html from 'components/Html';
+import Root from 'containers/Root';
+import configureStore from 'http/configureStore';
+
+import type { Context } from 'koa';
+
+export default () => (ctx: Context) => {
+  const routerContext = {};
+  const store = configureStore();
+
+  const html = renderToString(
+    <Provider store={store}>
+      <Router context={routerContext} location={ctx.req.url}>
+        <Root />
+      </Router>
+    </Provider>,
+  );
+
+  if (routerContext.url) {
+    ctx.redirect(routerContext.url);
+    return;
+  }
+
+  if (routerContext.status) {
+    ctx.status = routerContext.status;
+  }
+
+  const markup = renderToStaticMarkup(<Html html={html} />);
+  ctx.body = `<!doctype>${markup}`;
+};
