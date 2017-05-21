7fe9806df443fbcea8aa399f9bbd243bee431176
Christian de Botton <cdebotton@gmail.com>
2017-05-21 12:45:00 -0400

    Clean up input and label styles

--- libs/components/atoms/Input.jsx
+++ libs/components/atoms/Input.jsx
@@ -5,18 +5,23 @@ import { placeholder, rem, modularScale } from 'polished';
 
 const Input = styled.input`
   background-color: transparent;
-  border: none;
-  border-bottom: 1px solid #fff;
   padding: ${modularScale(-2)} ${modularScale(-1)};
-  color: #fff;
   font-size: ${modularScale(0)};
   letter-spacing: ${rem('1px')};
+  color: #ddd;
+  border: none;
+  border-bottom: 2px solid #ddd;
+
+  &:focus {
+    color: #fff;
+    border-bottom: 2px solid #fff;
+  }
 
   ${props => placeholder({
     fontFamily: props.theme.typography.fonts.condensed,
     fontWeight: 300,
     textTransform: 'uppercase',
-    letterSpacing: rem('2px'),
+    letterSpacing: rem('4px'),
     color: '#eee',
     fontSize: modularScale(0),
   })}
--- libs/components/atoms/Label.jsx
+++ libs/components/atoms/Label.jsx
@@ -1,14 +1,25 @@
 /* @flow */
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { modularScale, rem } from 'polished';
 import Input from 'components/atoms/Input';
 
+const showLabel = css`
+  opacity: 1;
+  transform: translate3d(0, 0, 0);
+  transition:
+    opacity 175ms ease-in,
+    transform 175ms ease-in;
+`;
+
 const Label = styled.label`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
   opacity: 0;
   transform: translate3d(0, -5px, 0);
   font-size: ${modularScale(-1)};
-  padding: ${modularScale(-2)} ${modularScale(-1)};
+  padding: ${modularScale(-2)} 0 0 ${modularScale(0)};
   font-weight: 300;
   text-transform: uppercase;
   letter-spacing: ${rem('2px')};
@@ -18,13 +29,12 @@ const Label = styled.label`
   transition:
     opacity 175ms ease-out,
     transform 175ms ease-out;
+  
+  ${props => props.active && showLabel}
+  ${Input}:not(:placeholder-shown) + & { ${showLabel} }
 
-  ${Input}:not(:placeholder-shown) + & {
-    opacity: 1;
-    transform: translate3d(0, 0, 0);
-    transition:
-      opacity 175ms ease-in,
-      transform 175ms ease-in;
+  & > *:first-child {
+    margin-right: auto;
   }
 `;
 
