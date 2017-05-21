c7109b53c503c84c4fb8b4fdf281cf4194b53dd9
Christian de Botton <cdebotton@gmail.com>
2017-05-21 12:45:14 -0400

    Display warning and error badges on labeled input

--- libs/components/molecules/LabeledInput.jsx
+++ libs/components/molecules/LabeledInput.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Field from 'components/atoms/Field';
 import Input from 'components/atoms/Input';
 import Label from 'components/atoms/Label';
+import Badge from 'components/atoms/Badge';
 import type { InputProps } from 'redux-form';
 
 export type Props = {
@@ -13,7 +14,11 @@ export type Props = {
 const LabeledInput = ({ label, input, meta, ...rest }: Props): React$Element<any> => (
   <Field>
     <Input placeholder={label} {...rest} {...input} />
-    <Label>{(meta.touched && meta.error) || label}</Label>
+    <Label active={meta.touched && (meta.warning || meta.error)}>
+      <span>{label}</span>
+      {meta.touched && meta.warning && <Badge warning>{meta.warning}</Badge>}
+      {meta.touched && meta.error && <Badge error>{meta.error}</Badge>}
+    </Label>
   </Field>
 );
 
