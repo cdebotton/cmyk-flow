8ecfb5f7ef6b958a18b0512af2c3fec7f823bf48
Christian de Botton <cdebotton@gmail.com>
2017-05-21 21:46:27 -0400

    Run session saga

--- libs/client/configureStore.js
+++ libs/client/configureStore.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from 'state';
+import sessionSaga from 'state/session/sagas';
 import type { Store } from 'state/types';
 
 const configureStore = (): Store => {
@@ -18,6 +19,8 @@ const configureStore = (): Store => {
       store.replaceReducer(nextRootReducer);
     });
   }
+
+  sagaMiddleware.run(sessionSaga);
 
   return store;
 };
