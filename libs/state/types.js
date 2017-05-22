c97b6bb7a4ed8b4facaa2cf9644cf5e1763c6319
Christian de Botton <cdebotton@gmail.com>
2017-05-21 22:48:40 -0400

    Correctly stube out state (except for forms).

--- libs/state/types.js
+++ libs/state/types.js
@@ -2,11 +2,15 @@
 
 import Reducer from 'state';
 import type { Dispatch as ReduxDispatch, Store as ReduxStore } from 'redux';
-import type { Action as SessionAction } from 'state/session/types';
+import type { Action as SessionAction, State as SessionState } from 'state/session/types';
 
-type $ExtractFunction = <V, A>({ [reducerName: string]: (state: V, action: A) => V }) => V;
+type $ExtractFunction = <V, A>({ form: (state: V, action: A) => V }) => V;
 
 export type Action = SessionAction;
 export type Dispatch = ReduxDispatch<Action>;
-export type State = $ObjMap<Reducer, $ExtractFunction>;
+type FormState = $ObjMap<Reducer, $ExtractFunction>;
+export type State = {
+  form: FormState,
+  session: SessionState,
+};
 export type Store = ReduxStore<State, Action>;
