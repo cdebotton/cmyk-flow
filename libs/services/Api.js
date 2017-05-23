3db782a90634f1c9a3a243849190df1048a0b204
Christian de Botton <cdebotton@gmail.com>
2017-05-23 00:49:25 -0400

    Create an API service

---
+++ libs/services/Api.js
@@ -0,0 +1,31 @@
+/* @flow */
+
+import fetch from 'isomorphic-fetch';
+
+type JSONType = string | number | { [key: string]: JSONType } | JSONType[];
+type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+
+const makeRequest = async (method: Method, uri: string, data: JSONType): Promise<JSON | void> => {
+  const response = await fetch(uri, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      include: 'credentials',
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    const { error } = await response.json();
+    throw new Error(error);
+  }
+
+  const result = await response.json();
+
+  return result;
+};
+
+export default {
+  login: (username: string, password: string) =>
+    makeRequest('POST', '/api/login', { username, password }),
+};
