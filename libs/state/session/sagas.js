a34198e963de3f6404bde0283b2a21153f098b37
Christian de Botton <cdebotton@gmail.com>
2017-05-23 00:49:35 -0400

    Use API service for auth

--- libs/state/session/sagas.js
+++ libs/state/session/sagas.js
@@ -1,30 +1,26 @@
 /* @flow */
 
-import fetch from 'isomorphic-fetch';
 import { call, put, takeLatest } from 'redux-saga/effects';
 import { LOGIN_REQUEST, loginSuccess, loginFailure } from 'state/session/actions';
+import Api from 'services/Api';
 import type { IOEffect } from 'redux-saga/effects';
 import type { Action } from 'state/types';
 
-const postLogin = (username: string, password: string): Promise<{ token: string }> =>
-  fetch('/api/login', {
-    method: 'POST',
-    body: JSON.stringify({ username, password }),
-  }).then(resp => resp.json());
-
-export function* loginRequested(action: Action): Generator<IOEffect, void, string> {
+export function* loginRequested(action: Action): Generator<IOEffect, void, *> {
   if (action.type !== LOGIN_REQUEST) {
     throw new TypeError(`Invalid action. Expected ${LOGIN_REQUEST}, not ${action.type}.`);
   }
 
+  const { username, password } = action.payload;
+
   try {
-    const token = yield call(postLogin, action.payload.username, action.payload.password);
+    const { token } = yield call(Api.login, username, password);
     yield put(loginSuccess(token));
   } catch (err) {
-    yield put(loginFailure(err));
+    yield put(loginFailure(err.message));
   }
 }
 
-export default function* sessionSaga(): Generator<IOEffect, void, string> {
+export default function* sessionSaga(): Generator<IOEffect, void, *> {
   yield takeLatest(LOGIN_REQUEST, loginRequested);
 }
