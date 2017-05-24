a9a0066c80c19e4fed0a49e9de504c019a5029ee
Christian de Botton <cdebotton@gmail.com>
2017-05-23 21:51:58 -0400

    Include router reducer in root reducer

--- libs/state/index.js
+++ libs/state/index.js
@@ -3,10 +3,12 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import session from 'state/session';
+import router from 'state/router';
 
 const rootReducer = combineReducers({
   form,
   session,
+  router,
 });
 
 export default rootReducer;
