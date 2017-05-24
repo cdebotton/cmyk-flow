5d1de7de821e7a51a437e8cea2c42a77ebb4596e
Christian de Botton <cdebotton@gmail.com>
2017-05-23 21:51:27 -0400

    Create router reducer

---
+++ libs/state/router/index.js
@@ -0,0 +1,23 @@
+/* @flow */
+
+import { LOCATION_CHANGE } from 'state/router/actions';
+import type { State } from 'state/router/types';
+import type { Action } from 'state/types';
+
+const initialState = {
+  location: null,
+};
+
+const reducer = (state: State = initialState, action: Action) => {
+  switch (action.type) {
+    case LOCATION_CHANGE:
+      return {
+        ...state,
+        location: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
