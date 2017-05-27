fb31e69901914d70118e6451aee00906c86a699d
Christian de Botton <cdebotton@gmail.com>
2017-05-27 13:24:00 -0400

    Add admin route as Private Route

--- libs/containers/Root.jsx
+++ libs/containers/Root.jsx
@@ -5,12 +5,15 @@ import Switch from 'react-router/Switch';
 import Route from 'react-router/Route';
 import App from 'components/atoms/App';
 import Login from 'containers/Login';
+import Admin from 'components/pages/Admin';
 import NotFound from 'components/pages/NotFound';
+import PrivateRoute from 'containers/PrivateRoute';
 
 const Root = (): React$Element<any> => (
   <App>
     <Switch>
       <Route path="/login" component={Login} />
+      <PrivateRoute path="/admin" component={Admin} />
       <Route component={NotFound} />
     </Switch>
   </App>
